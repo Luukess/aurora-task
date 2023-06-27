dc9010775ae1f7d7110e27c585345f2a38caea7b
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-06-27 18:34:20 +0200

    Feat: added sidebar nav, handled click event to show and hide sidebar

---
+++ scripts/navMobile.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    const mobileNavBackground = document.getElementById('mobile-nav-background');
+    const sidebarContainer = document.getElementById('sidebar-container');
+    const sidebar = document.getElementById('sidebar');
+
+    document.onclick = function (e) {
+        if (e.target.className === 'icon-menu hamburger-icon-menu') {
+            mobileNavBackground.style.width = '100%';
+            sidebarContainer.style.width = '80%';
+            sidebar.style.opacity = '1';
+            sidebar.style.transition = 'opacity 2s';
+        } else if (e.target.id === 'mobile-nav-background') {
+            mobileNavBackground.style.width = '0%';
+            sidebarContainer.style.width = '0%';
+            sidebar.style.opacity = '0';
+            sidebar.style.transition = 'opacity 0.2s';
+        };
+    };
+};
