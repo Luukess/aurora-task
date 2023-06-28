6e90387f7adc5e78a2600717d0a2465cb32e6a9e
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-06-28 16:53:05 +0200

    Feat: added media for mobile nav

--- scripts/navMobile.js
+++ scripts/navMobile.js
@@ -1,19 +1,26 @@
 window.onload = function () {
+    const mobileNavWrapper = document.getElementById('mobile-nav-wrapper');
     const mobileNavBackground = document.getElementById('mobile-nav-background');
     const sidebarContainer = document.getElementById('sidebar-container');
     const sidebar = document.getElementById('sidebar');
 
     document.onclick = function (e) {
         if (e.target.className === 'icon-menu hamburger-icon-menu') {
-            mobileNavBackground.style.width = '100%';
-            sidebarContainer.style.width = '80%';
-            sidebar.style.opacity = '1';
-            sidebar.style.transition = 'opacity 2s';
+            setTimeout(() => {
+                mobileNavBackground.style.width = '100%';
+                sidebarContainer.style.width = '80%';
+                sidebar.style.opacity = '1';
+                sidebar.style.transition = 'opacity 2s';
+            }, 100);
+            mobileNavWrapper.style.display = 'block';
         } else if (e.target.id === 'mobile-nav-background' || e.target.className === 'icon-cancel close-icon') {
             mobileNavBackground.style.width = '0%';
             sidebarContainer.style.width = '0%';
             sidebar.style.opacity = '0';
             sidebar.style.transition = 'opacity 0.2s';
+            setTimeout(() => {
+                mobileNavWrapper.style.display = 'none';
+            }, 300);
         }
     };
 };
